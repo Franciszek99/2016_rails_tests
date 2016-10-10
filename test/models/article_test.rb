7f973b1c8140a879d614fae5cc08e4265645dd45
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-10 12:42:51 +0000

    test2, failed

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,11 +1,9 @@
 require 'test_helper'#need it
 
 class ArticleTest < ActiveSupport::TestCase
-  test "should not save article without title" do
-    article = Article.new
-    assert_not article.save
-  end
-  class Article < ApplicationRecord
-    validates :title, presence: true
-  end
+  test "should report error" do
+  # some_undefined_variable is not defined elsewhere in the test case
+  some_undefined_variable
+  assert true
+end
 end
